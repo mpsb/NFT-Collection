bcda092ba614b2112f7ab2e5fbbef895fb6717d1
mpsb <mps.bilo@gmail.com>
2022-02-27 20:53:21 +0800

    init commit

---
+++ my-app/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x7FD3E10eA655d8a02324908d2569c79751ABfE13";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
